9c1922b013ae0a7482df104338d8a7c282335a22
Arif0004 <arifmahmudaj6@gmail.com>
2022-02-27 14:18:36 +0600

    template

---
+++ routes/admin.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->name('admin')->group(function () {
+    Route::view('test','layouts.backend_app');
+
+    Route::view('login','backend.auth.login');
+    Route::view('dashboard','backend.dashboard');
+
+});
+
+
+
